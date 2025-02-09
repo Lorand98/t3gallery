07becca3da0e316369fa4e717117f4ad36a012b4
Lorand98 <kalmarlorand@yahoo.com>
2025-02-09 21:03:53 +0200

    initial setup auth with clerk

---
+++ src/_components/topnav.tsx
@@ -0,0 +1,23 @@
+import {
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    UserButton
+} from "@clerk/nextjs";
+
+export function TopNav() {
+  return (
+    <nav className="b-4 flex items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,40 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (shape, { desc }) => desc(shape.createdAt),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img
+            src={image.url}
+            alt={image.name}
+            className="h-full w-full rounded-lg object-cover"
+          />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img
-              src={image.url}
-              alt={image.name}
-              className="h-full w-full rounded-lg object-cover"
-            />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="p-8 text-center text-2xl">
+          Please sign in above to see images
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
