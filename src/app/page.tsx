f633009fa39727a5a103f58906691893e6896cfb
Lorand98 <kalmarlorand@yahoo.com>
2025-02-09 18:34:12 +0200

    read url from db and render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,23 @@
-import fs from "fs";
-import path from "path";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  // This needs to be server-side as it reads from the filesystem
-  const imageDirectory = path.join(process.cwd(), "public/images");
-  const imageFiles = fs.readdirSync(imageDirectory).map((image, index) => ({
-    id: index + 1,
-    source: image,
-  }));
+  const images = await db.query.images.findMany({
+    orderBy: (shape, { desc }) => desc(shape.createdAt),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...imageFiles, ...imageFiles, ...imageFiles].map((image, idx) => (
-          <div key={image.id + "-" + idx} className="w-48">
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
             <img
-              src={`/images/${image.source}`}
-              alt={image.source}
+              src={image.url}
+              alt={image.name}
               className="h-full w-full rounded-lg object-cover"
             />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
